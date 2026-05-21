bebd4c97090183368b19202d842304573612b893
Arslan khan <your@gmail.com>
2026-05-21 23:51:25 +0500

    fix page imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,17 @@
-'use client';
-
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import AboutUs from '@/components/AboutUs';
-import AcademicPrograms from '@/components/AcademicPrograms';
-import ProfessionalCourses from '@/components/ProfessionalCourses';
-import Faculty from '@/components/Faculty';
-import ResultsAchievements from '@/components/ResultsAchievements';
-import Testimonials from '@/components/Testimonials';
-import FeeStructure from '@/components/FeeStructure';
-import ContactForm from '@/components/ContactForm';
-import Footer from '@/components/Footer';
+import Navbar from '../../app/components/Navbar';
+import Hero from '../../app/components/Hero';
+import Courses from '../../app/components/Courses';
+import Contact from '../../app/components/Contact';
+import Footer from '../../app/components/Footer';
 
 export default function Home() {
   return (
-    <div className="font-sans antialiased">
+    <main>
       <Navbar />
-      <main>
-        <Hero />
-        <AboutUs />
-        <AcademicPrograms />
-        <ProfessionalCourses />
-        <Faculty />
-        <ResultsAchievements />
-        <Testimonials />
-        <FeeStructure />
-        <ContactForm />
-      </main>
+      <Hero />
+      <Courses />
+      <Contact />
       <Footer />
-    </div>
+    </main>
   );
 }
