113dbdbedf45b712c70fc30935f5bbfe09ad54d7
Arslan khan <your@gmail.com>
2026-05-21 23:56:17 +0500

    fix src page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Navbar from '../../app/components/Navbar';
-import Hero from '../../app/components/Hero';
-import Courses from '../../app/components/Courses';
-import Contact from '../../app/components/Contact';
-import Footer from '../../app/components/Footer';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Courses from '../components/Courses';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
